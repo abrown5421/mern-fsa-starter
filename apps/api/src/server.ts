67cbe2cd858a2c1a4002a01de06af137f1748df4
abrown5421 <augustashtonbrown@gmail.com>
2026-01-05 13:06:57 -0500

    express doesnt use wildcard anymore so I refactored that to use a different approach

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -29,12 +29,16 @@ app.use(
   })
 );
 
-app.options('*', cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 
