0c277b35ef0e5225f16c3a1964437988a6a7433e
abrown5421 <augustashtonbrown@gmail.com>
2026-01-05 14:48:15 -0500

    removing cookie domain flag

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -65,7 +65,6 @@ app.use(
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7,
-      domain: process.env.NODE_ENV === "production" ? ".onrender.com" : undefined,
     },
   })
 );
